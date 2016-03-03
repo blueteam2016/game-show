29024dd48fad21de3d557abde1ca9af5d8c68cde
Varun Chitkara <varun.chitkara@gmail.com>
2016-03-03 12:46:50 -0500

    Added classes to main file

---
+++ GameShow/src/com/blueteam/gameshow/client/ClientWindow.java
@@ -0,0 +1,26 @@
+package com.blueteam.gameshow.client;
+import java.awt.HeadlessException;
+import java.awt.Window;
+
+public class ClientWindow{
+	
+	ClientProfile profile;
+	ClientIO clientIO;
+	RegistrationScreen rScreen;
+	ClientQuestionScreen cqScreen;
+	String pathToFolder;
+
+	ClientWindow(){
+		profile=new ClientProfile();
+		clientIO=new ClientIO(pathToFolder);
+		rScreen=new RegistrationScreen(profile);
+		cqScreen=new ClientQuestionScreen();
+		
+	}
+	
+	public void register(){
+		
+	}
+	
+
+}
