658d60e7439ac8dbca559c705e35bcc41fea6102
Aidan <Aidan@MININT-DGPB17D>
2016-03-03 15:58:43 -0500

    A version of the Quiz that *should* work and an updated Question Class that takes both time and pointValue in its constructor

--- GameShow/src/com/blueteam/gameshow/data/Quiz.java
+++ GameShow/src/com/blueteam/gameshow/data/Quiz.java
@@ -13,6 +13,7 @@ import org.w3c.dom.*;
 public class Quiz {
 		
 	private ArrayList<Question> questions;
+	private Answer[] answers;
 	private int questionNum;
 	
 	public Quiz(String filePath)
@@ -22,7 +23,35 @@ public class Quiz {
 		try{
 			DocumentBuilder quizBuilder = quizBuilderFactory.newDocumentBuilder();
 			quizFile = quizBuilder.parse(filePath);
-		}catch(Exception e){System.out.println("THE QUIZ FILE WAS NOT BUILT CORRECTLY");}
+			quizFile.getDocumentElement().normalize();
+			Element root = quizFile.getDocumentElement();
+			NodeList questionList = root.getElementsByTagName("item");
+			for(int index=0; index < questionList.getLength();index++)
+			{
+				Node currentQuestion = questionList.item(index);
+				if(currentQuestion.getNodeType() == Node.ELEMENT_NODE)
+				{
+					Element questionElement = (Element) currentQuestion;
+					NodeList answerList = questionElement.getElementsByTagName("answer");
+					answers = new Answer[answerList.getLength()];
+					for(int answerIndex=0;answerIndex<answerList.getLength();answerIndex++)
+					{
+						Node currentAnswer = answerList.item(answerIndex);
+						if(currentAnswer.getNodeType() == Node.ELEMENT_NODE)
+						{
+							Element answerElement = (Element)currentAnswer;
+							answers[answerIndex]=new Answer(answerElement.getTextContent(),answerElement.getAttribute("correct").equals("true"));
+						}
+					}
+					String questionString =questionElement.getFirstChild().getTextContent();
+					String explanation = questionElement.getElementsByTagName("explanation").item(0).getTextContent();
+					int pointValue = Integer.parseInt(questionElement.getElementsByTagName("point_value").item(0).getTextContent());
+					int time = Integer.parseInt(questionElement.getElementsByTagName("time").item(0).getTextContent());
+					questions.add(new Question(questionString,answers,explanation,pointValue,time));
+				}
+			}
+			
+		}catch(Exception e){System.out.println("THE QUIZ FILE WAS NOT BUILT CORRECTLY");e.printStackTrace();}
 		questions = new ArrayList<Question>();
 		questionNum = 0;
 	}
