04adc719e652375e8e8553b9e9d824e348a86f7b
tzong4412 <tzong4412@CHHB45C4282D77.inst.hcpss.org>
2016-03-03 13:18:08 -0500

    asdf

--- GameShow/src/com/blueteam/gameshow/client/ClientQuestionMode.java
+++ GameShow/src/com/blueteam/gameshow/client/ClientQuestionMode.java
@@ -1,5 +1,8 @@
 package com.blueteam.gameshow.client;
 import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class ClientQuestionMode extends JPanel
@@ -7,6 +10,7 @@ public class ClientQuestionMode extends JPanel
 	private Question question;
 	private JLabel questionText;
 	private ArrayList<Answer> answerChoices;
+	private ArrayList<AnswerSelect> answerButtons;
 	private JPanel displayAnswers;
 	
 	ClientQuestionMode(ClientIO c, ClientQuestionScreen qs)
@@ -19,7 +23,21 @@ public class ClientQuestionMode extends JPanel
 		displayAnswers.setLayout(new BoxLayout(displayAnswers, BoxLayout.Y_AXIS));
 		
 		for(int i = 0; i < answerChoices.size(); i++)
+		{
+			JButton answerSelect = new AnswerButton();
+			answerSelect.addActionListener(this);
 			
+			JPanel answer = new JPanel();
+			answer.setLayout(new BoxLayout(answer, BoxLayout.X_AXIS));
+			answer.add();
+		}
 		
+	}
+	class AnswerButton extends JButton implements ActionListener
+	{
+		public void actionPerformed(ActionEvent arg0)
+		{
+			
+		}
 	}
 }
