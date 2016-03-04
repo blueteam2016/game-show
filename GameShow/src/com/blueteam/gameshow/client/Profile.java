a2cd7dfd1f06e3cc8a9cd0c44275d739e81a0b36
Daniel Kelly <dk97@comcast.net>
2016-03-04 13:01:32 -0500

    Added private keyword in Profile class.

--- GameShow/src/com/blueteam/gameshow/client/Profile.java
+++ GameShow/src/com/blueteam/gameshow/client/Profile.java
@@ -1,58 +1,65 @@
 package com.blueteam.gameshow.client;
 
 public class Profile {
+	private String servFoldLoc;
+	private String clientFoldLoc;
+	private int defVal;
+	private int defTime;
+	private String qFileLoc;
+	private boolean foundSavedFile;
 
-	String servFoldLoc;
-	String clientFoldLoc;
-	int defVal;
-	int defTime;
-	String qFileLoc;
-	boolean foundSavedFile;
-
-	Profile(){
+	public Profile(){
 		//Get Aidan
 	}
 
 	public void saveProfile(){
 		//Get Daniel
 	}
-
-
+	
 	public String getServerFolderLoc(){
 		return servFoldLoc;
 	}
+	
 	public String getClientFolderLoc(){
 		return clientFoldLoc;
 	}
+	
 	public String getQuestionFileLoc(){
 		return qFileLoc;
 	}
+	
 	public int getDefaultValue(){
 		return defVal;
 	}
+	
 	public int getDefaultTime(){
 		return defTime;
 	}
+	
 	public boolean savedFileFound(){
 		return foundSavedFile;
 	}
 
-
 	public void setServerFolderLoc(String loc){
 		servFoldLoc=loc;
 	}
+	
 	public void setClientFolderLoc(String loc){
 		clientFoldLoc=loc;
 	}
+	
 	public void setQuestionFileLoc(String loc){
 		qFileLoc=loc;
 	}
+	
 	public void setDefaultValue(int v){
 		defVal=v;
 	}
+	
 	public void setDefaultTime(int t) {
 		defTime=t;
 	}
+	
 	public boolean isComplete(){
 		if (!servFoldLoc.equals(""))
 			if (!clientFoldLoc.equals(""))
