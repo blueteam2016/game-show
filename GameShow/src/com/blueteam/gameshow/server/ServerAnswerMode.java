ab2c67d6562ed529ff075936b1a6cafc9df9faf7
Sahil Saini <sssaini011@gmail.com>
2016-03-04 12:57:12 -0500

    answer mode done sans the actual gui bit but all the internal stuff should work

--- GameShow/src/com/blueteam/gameshow/server/ServerAnswerMode.java
+++ GameShow/src/com/blueteam/gameshow/server/ServerAnswerMode.java
@@ -1,7 +1,43 @@
 package com.blueteam.gameshow.server;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
-public class ServerAnswerMode extends JPanel{
+public class ServerAnswerMode extends JPanel implements ActionListener{
 
+	private JLabel question;
+	private ArrayList<JLabel> answers;
+	private JLabel explanation;
+	private JButton moveOn;
+	
+	
+	public ServerAnswerMode(Game g, ServerGameScreen s){
+		
+		question = new JLabel(g.getQuiz().getCurrentQuestion().getQuestionText());
+		
+		answers = new ArrayList<JLabel>();
+		
+		for(int x = 0; x< g.getQuiz().getCurrentQuestion().getAnswers().size(); x++){
+			if(g.getQuiz().getCurrentQuestion().getAnswer(x).isCorrect())
+				answers.add(g.getQuiz().getCurrentQuestion().getAnswer(x));	
+		}
+		
+		moveOn = new JButton("Continue");
+		moveOn.addActionListener(this);
+		
+		
+	}
+
+
+	public void actionPerformed(ActionEvent arg0) {
+		
+		goToResultMode();
+		
+	}
+	
 }
