78bf30c9508c9b9a8d6c8a78717ed1be01d265d4
Sahil Saini <sssaini011@gmail.com>
2016-03-07 13:05:41 -0500

    ServerAnswerMode constructor complete, shouldnt break now

--- GameShow/src/com/blueteam/gameshow/server/ServerAnswerMode.java
+++ GameShow/src/com/blueteam/gameshow/server/ServerAnswerMode.java
@@ -17,18 +17,31 @@ public class ServerAnswerMode extends JPanel implements ActionListener{
 	
 	
 	public ServerAnswerMode(Game g, ServerGameScreen s){
+question = new JLabel(g.getQuiz().getCurrentQuestion().getQuestionText());
 		
-		question = new JLabel(g.getQuiz().getCurrentQuestion().getQuestionText());
+		explanation = newJLabel("Explanation: " + g.getQuiz().getCurrentQuestion().getExplanation());
 		
 		answers = new ArrayList<JLabel>();
 		
 		for(int x = 0; x< g.getQuiz().getCurrentQuestion().getAnswers().size(); x++){
 			if(g.getQuiz().getCurrentQuestion().getAnswer(x).isCorrect())
-				answers.add(new JLabel(g.getQuiz().getCurrentQuestion().getAnswer(x)));	
+				answers.add(new JLabel((char)(65+x) + ") " + g.getQuiz().getCurrentQuestion().getAnswer(x)));	
 		}
 		
 		moveOn = new JButton("Continue");
 		moveOn.addActionListener(this);
+		
+		add(question);
+		for(int y = 0; y< answers.size(); y++){
+			
+			add(answers.get(y));
+			
+		}
+		
+		add(explanation);
+		
+		
+		add(moveOn);
 		
 		
 	}
