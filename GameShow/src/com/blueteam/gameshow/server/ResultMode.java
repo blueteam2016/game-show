3a75d0e5d52821fcb89888cf15fb7c6ec753242f
hkuo0210 <hkuo0210@CHHB45C428255C.inst.hcpss.org>
2016-03-07 13:00:23 -0500

    Added Player, Roster, Team, ResultMode, ScoreboardScreen and corresponding tableModels

---
+++ GameShow/src/com/blueteam/gameshow/server/ResultMode.java
@@ -0,0 +1,55 @@
+package com.blueteam.gameshow.server;
+
+import java.awt.BorderLayout;
+import java.awt.event.*;
+
+import javax.swing.*;
+import javax.swing.table.*;
+
+
+public class ResultMode extends JPanel{
+	private ResultTableModel model;
+	private JTable table;
+	private JButton back;
+	private JButton nextQ;
+	
+	public ResultMode(Game g, ServerGameScreen s){
+		model = new ResultTableModel(g.getRoster());
+		table = new JTable(model);
+		
+		back = new JButton("Back");
+		back.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				s.goToAnswerMode();
+			}
+		});
+		
+		nextQ = new JButton("Next Question");
+		nextQ.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				s.goToQuestionMode();
+			}
+		});
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(back);
+		buttonPanel.add(nextQ);
+		
+		setLayout(new BorderLayout());
+		add(table, BorderLayout.CENTER);
+		add(table.getTableHeader(), BorderLayout.NORTH);
+		add(buttonPanel, BorderLayout.SOUTH);
+	}
+	
+	public void lastQuestion(){
+		nextQ.setEnabled(false);
+	}
+	
+	public void update(){
+		table.revalidate();
+		repaint();
+	}
+	
+	
+	
+}
---
+++ GameShow/src/com/blueteam/gameshow/server/ResultTableModel.java
@@ -0,0 +1,40 @@
+package com.blueteam.gameshow.server;
+
+
+import javax.swing.table.*;
+
+import data.*;
+
+
+public class ResultTableModel extends AbstractTableModel{
+	private Roster rost;
+
+	public ResultTableModel(Roster r){
+		rost = r;
+	}
+
+	public String getColumnName(int col){
+		if(col==0){
+			return "Team Name";
+		}else{
+			return "Percentage";
+		}
+	}
+
+	public int getColumnCount() {
+		return 2;
+	}
+
+	public int getRowCount() {
+		return rost.numTeams();
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		if(columnIndex == 0){
+			return rost.getTeam(rowIndex).getName();
+		}else{
+			return rost.getTeam(rowIndex).getPercentage();
+		}
+	}
+}
+
