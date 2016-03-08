462381bed6c8ce0f335370970ffc68de6c1580e7
Tom Zong <t.zong3.1@gmail.com>
2016-03-08 12:51:20 -0500

    ClientAnswerMode reverted to updated version

--- GameShow/src/com/blueteam/gameshow/client/ClientAnswerMode.java
+++ GameShow/src/com/blueteam/gameshow/client/ClientAnswerMode.java
@@ -1,11 +1,25 @@
 package com.blueteam.gameshow.client;
 import javax.swing.*;
+
+import com.blueteam.gameshow.client.ClientQuestionMode.AnswerButton;
+import com.blueteam.gameshow.data.Answer;
+import com.blueteam.gameshow.data.Question;
+
 import java.util.ArrayList;
 
 public class ClientAnswerMode extends JPanel
 {
-	public ClientAnswerMode(ClientQuestionScreen qs)
+	private Question question;
+	private JLabel questionText;
+	private Answer[] answerChoices;
+	private ArrayList<AnswerButton> answerButtons;
+	private JPanel displayAnswers;
+	
+	ClientAnswerMode(ClientQuestionScreen qs)
 	{
+		question = qs.getQuestion;
+		questionText = new JLabel(question.getText());
+		answerChoices = question.getAnswers();
 		
 	}
 }
