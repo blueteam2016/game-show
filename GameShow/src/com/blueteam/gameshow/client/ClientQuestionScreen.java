348fc21dbee889e8ec0ebf003d2e19bf4422968d
kwhite6493 <kellymw98@gmail.com>
2016-03-08 12:45:37 -0500

    added clientQuestionScreen

---
+++ GameShow/src/com/blueteam/gameshow/client/ClientQuestionScreen.java
@@ -0,0 +1,49 @@
+package com.blueteam.gameshow.client;
+import javax.swing.JPanel;
+
+
+public class ClientQuestionScreen {
+private ClientQuestionMode cQuestion;
+private ClientAnswerMode cAnswer;
+private JPanel noQuestionAvailable;
+private JPanel notRegistered;
+private JPanel currentMode;
+private Question currentQuestion;
+
+ClientIO clientIO;
+
+public ClientQuestionScreen(){
+	
+	clientIO = new ClientIO();
+	cQuestion = new ClientQuestionMode(clientIO, this);
+	cAnswer = new ClientAnswerMode(this);
+	noQuestionAvailable = new JPanel();
+	notRegistered = new JPanel();
+	currentMode = new JPanel();
+	//parameters: string qText, Answer[] ans, String exp, int t, intv
+	currentQuestion = new Question();
+	
+	
+}
+public void goToAnswerMode(){
+	currentMode= cAnswer;
+}
+public void goToQuestionMode(){
+	currentMode = cQuestion; 
+}
+public void goToNotRegistered(){
+	currentMode = notRegistered;
+}
+public void goToNoQuestion(){
+	currentMode = noQuestionAvailable;
+}
+public Question getQuestion(){
+	return currentQuestion;
+}
+public JPanel getCurrentMode(){
+	return currentMode;
+}
+
+
+
+}
