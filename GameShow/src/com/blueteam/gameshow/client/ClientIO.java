0da79454614ea4fec1812b2f2b4f59a0264a3039
Daniel Kelly <dk97@comcast.net>
2016-03-08 23:11:32 -0500

    Now truncates files before use.

--- GameShow/src/com/blueteam/gameshow/client/ClientIO.java
+++ GameShow/src/com/blueteam/gameshow/client/ClientIO.java
@@ -16,11 +16,23 @@ public class ClientIO {
 	private ObjectInputStream questIn;
 	private ObjectOutputStream ansOut;
 	
+	private void truncate(FileOutputStream out) {
+		try {
+			out.getChannel().truncate(0);
+			out.getChannel().force(true);
+			out.getChannel().lock();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public ClientIO(String pathServerFold, String pathClientFold, ClientProfile profile) {
 		String identifier = profile.getIdentifier();
 
 		try {
-			ObjectOutputStream profOut = new ObjectOutputStream(new FileOutputStream(pathClientFold + ".profile_" + identifier));
+			FileOutputStream fOut = new FileOutputStream(pathClientFold + ".profile_" + identifier);
+			truncate(fOut);
+			ObjectOutputStream profOut = new ObjectOutputStream(fOut);
 			profOut.writeObject(profile);
 			profOut.close();
 		} catch (IOException e) {
@@ -30,13 +42,16 @@ public class ClientIO {
 		try {
 			questIn = new ObjectInputStream(new FileInputStream(pathServerFold + ".question"));
 		} catch (FileNotFoundException e) {
-			// error handling
+			System.out.println("File not found.");
+			//TODO: error handling
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		try {
-			ansOut = new ObjectOutputStream(new FileOutputStream(pathClientFold + ".answer_" + identifier));
+			FileOutputStream fOut = new FileOutputStream(pathClientFold + ".answer_" + identifier);
+			truncate(fOut);
+			ansOut = new ObjectOutputStream(fOut);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -60,6 +75,7 @@ public class ClientIO {
 	public void sendAnswer(Answer answer) {
 		try {
 			ansOut.writeObject(answer);
+			ansOut.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- GameShow/src/com/blueteam/gameshow/server/ServerIO.java
+++ GameShow/src/com/blueteam/gameshow/server/ServerIO.java
@@ -1,77 +1,83 @@
-//package com.blueteam.gameshow.server;
-//
-//import java.io.EOFException;
-//import java.io.FileInputStream;
-//import java.io.FileNotFoundException;
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//import java.io.ObjectInputStream;
-//import java.io.ObjectOutputStream;
-//import java.net.InetAddress;
-//import java.net.UnknownHostException;
-//
-//import com.blueteam.gameshow.data.Answer;
-//import com.blueteam.gameshow.data.Question;
-//
-//public class ServerIO {
-//	private ObjectInputStream in;
-//	private ObjectOutputStream out;
-//	
-//	public ServerIO(String pathToFolder) {
-//		FileOutputStream fOut = null;
-//				try {
-//					fOut = new FileOutputStream(pathToFolder + ".question");
-//				} catch (FileNotFoundException e) {
-//					e.printStackTrace();
-//				}
-//				try {
-//					out = new ObjectOutputStream(fOut);
-//				} catch (IOException e) {
-//					e.printStackTrace();
-//				}
-//		
-//	}
-//	
-//	public void openIn(String pathToFolder) {
-//		FileInputStream fIn = null;
-//		String sysName = null;
-//		try {
-//			sysName = InetAddress.getLocalHost().getHostName(); //apparently the only way to get the machine hostname in Java
-//		} catch (UnknownHostException e1) {
-//			e1.printStackTrace();
-//		}
-//		try {
-//			fIn = new FileInputStream(pathToFolder + ".answer_" + sysName);
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		};
-//		try {
-//			in = new ObjectInputStream(fIn);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//
-//	public Answer getAnswer() {
-//		Answer qIn = null;
-//		try {
-//			qIn = (Answer)in.readObject();
-//		} catch (EOFException e) {
-//			return null;
-//		} catch (ClassNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		return qIn;
-//	}
-//	
-//	public void sendQuestion(Question question) {
-//				try {
-//					out.writeObject(question);
-//				} catch (IOException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//	
-//}
+package com.blueteam.gameshow.server;
+
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import com.blueteam.gameshow.data.Answer;
+import com.blueteam.gameshow.data.Question;
+
+public class ServerIO {
+	private ObjectInputStream in;
+	private ObjectOutputStream out;
+	
+	private void truncate(FileOutputStream out) {
+		try {
+			out.getChannel().truncate(0);
+			out.getChannel().force(true);
+			out.getChannel().lock();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ServerIO(String pathToFolder) {
+		try {
+			FileOutputStream fOut = new FileOutputStream(pathToFolder + ".question");
+			truncate(fOut);
+			out = new ObjectOutputStream(fOut);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void openIn(String pathToFolder) {
+		String sysName = null;
+		while (true) {
+			try {
+				sysName = InetAddress.getLocalHost().getHostName(); //apparently the only way to get the machine hostname in Java
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			}
+			try {
+				in = new ObjectInputStream(new FileInputStream(pathToFolder + ".answer_" + sysName));
+			} catch (FileNotFoundException e) {
+				continue;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		}
+	}
+
+	public Answer getAnswer() {
+		Answer qIn = null;
+		try {
+			qIn = (Answer)in.readObject();
+		} catch (EOFException e) {
+			return null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return qIn;
+	}
+	
+	public void sendQuestion(Question question) {
+				try {
+					out.writeObject(question);
+					out.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+	
+}
