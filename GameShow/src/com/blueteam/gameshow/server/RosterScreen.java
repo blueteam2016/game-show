af41c7401b80e4b2d09595f197ef89c8cd1593e9
Will Kraft <willkraft@comcast.net>
2016-03-09 12:31:28 -0500

    added RosterScreen class

--- GameShow/src/com/blueteam/gameshow/server/RosterScreen.java
+++ GameShow/src/com/blueteam/gameshow/server/RosterScreen.java
@@ -1,10 +1,8 @@
 package com.blueteam.gameshow.server;
 
-//will kraft
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
