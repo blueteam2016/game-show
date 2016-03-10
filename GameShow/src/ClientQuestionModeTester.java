88e3f15e0b1db12fcf3f61caba364a749cd4d257
Tom Zong <t.zong3.1@gmail.com>
2016-03-10 12:59:56 -0500

    started tester

---
+++ GameShow/src/ClientQuestionModeTester.java
@@ -0,0 +1,15 @@
+
+
+import javax.swing.*;
+
+import com.blueteam.gameshow.client.ClientWindow;
+
+public class ClientQuestionModeTester
+{
+	public static void main(String[] args)
+	{
+		ClientWindow window = new ClientWindow();
+		window.
+		
+	}
+}
