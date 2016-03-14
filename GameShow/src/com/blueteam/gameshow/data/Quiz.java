7f0aae0289b1ca7032cac257ef829d24ee4cc07c
Aidan Newell <am.newell@verizon.net>
2016-03-14 13:04:38 -0400

    A quiz change for time variable

--- GameShow/src/com/blueteam/gameshow/data/Quiz.java
+++ GameShow/src/com/blueteam/gameshow/data/Quiz.java
@@ -56,6 +56,9 @@ public class Quiz {
 				try{
 					time = Integer.parseInt(questionElement.getElementsByTagName("time").item(0).getTextContent());
 				}catch(Exception e){time = Profile.getDefaultTime();}
+				//Fine for now, sets time t=0 to default time
+				if(time > Profile.getDefaultTime())
+					time = Profile.getDefaultTime();
 				questions[index] = new Question(questionString,answers,explanation,pointValue,time);
 			}
 		}
