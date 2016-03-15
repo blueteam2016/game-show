8003c6ed84b8e68f2db8f74feba7d778a6ad2c42
Daniel Kelly <dk97@comcast.net>
2016-03-15 13:12:23 -0400

    Client now uses timers to get question.

--- GameShow/src/com/blueteam/gameshow/client/ClientQuestionMode.java
+++ GameShow/src/com/blueteam/gameshow/client/ClientQuestionMode.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class ClientQuestionMode extends JPanel implements Runnable
+public class ClientQuestionMode extends JPanel implements ActionListener
 {
 
 	private static final long serialVersionUID = 1473664480186370825L;
@@ -17,29 +17,28 @@ public class ClientQuestionMode extends JPanel implements Runnable
 	private ClientIO clientIO;
 	private Question question;
 	private JLabel questionText;
-	private JLabel timerLabel;
-	private int seconds;
-	private Timer timer;
 	private Answer[] answerChoices;
 	private ArrayList<AnswerButton> answerButtons;
 	private JPanel displayAnswers;
+	private Timer questionTimer;
 	
 	public ClientQuestionMode(ClientQuestionScreen qs)
 	{
 		clientWindow = qs.getClientWindow();
 		questScreen = qs;
+		questionTimer = new Timer(1, this);
 	}
 	
 	public void register() {
 		clientIO = clientWindow.getClientIO();
+		questionTimer.start();
 	}
 	
 	@Override
-	public void run() {
+	public void actionPerformed(ActionEvent arg0) {
 		while (true) {
 			question = clientIO.getQuestion();
-		
-			seconds = question.getTime();
+
 			questScreen.setQuestion(question);
 			this.removeAll();
 			questionText = new JLabel(question.getText());
@@ -61,44 +60,14 @@ public class ClientQuestionMode extends JPanel implements Runnable
 				answer.add(answerText);
 				displayAnswers.add(answer);
 			}
-			
-			timerLabel = new JLabel("Time Remaining: " + seconds/60 + ":" + secondsText(seconds));
-			timer = new Timer(1000, new ActionListener(){
-				public void actionPerformed(ActionEvent arg0){
-					seconds -= 1;
-					timerLabel.setText("Time Remaining: " + seconds/60 + ":" + secondsText(seconds));
-					if (seconds <= 0){
-						timer.stop();
-						questScreen.goToAnswerMode();
-					}
-				}	
-			});
+
 			this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 			this.add(questionText);
 			this.add(displayAnswers);
-			this.add(timerLabel);
 			questScreen.goToQuestionMode();
 		}
 	}
-	public void startTimer()
-	{	
-		timer.start();	
-	}	
-	public void stopTimer()
-	{		
-		timer.stop();
-	}
-	private static String secondsText(int seconds)
-	{
-		String secondsText = "";
-		if (seconds % 60 == 0)
-			secondsText = "00";
-		else if (seconds % 60 < 10)
-			secondsText = "0" + seconds;
-		else
-			secondsText = "" + seconds%60;
-		return secondsText;
-	}	
+
 	class AnswerButton extends JButton implements ActionListener
 	{
 		private static final long serialVersionUID = 688447363351164099L;
@@ -116,4 +85,5 @@ public class ClientQuestionMode extends JPanel implements Runnable
 			questScreen.goToAnswerMode();
 		}
 	}
+
 }
