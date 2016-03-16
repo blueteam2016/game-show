fb5a069351e65d0dcce3116afcbbb195e4f73a6f
hkuo0210 <hkuo0210@CHHB45C428255C.inst.hcpss.org>
2016-03-16 12:57:07 -0400

    fixed caps method in registrationScreen

--- GameShow/src/com/blueteam/gameshow/client/RegistrationScreen.java
+++ GameShow/src/com/blueteam/gameshow/client/RegistrationScreen.java
@@ -221,9 +221,29 @@ public class RegistrationScreen extends JPanel{
 
 	}
 
-	
+	private static String correctCaps(String phrase){
+		phrase += " ";
+		
+		ArrayList<String> words = new ArrayList<String>();
+		String correct = "";
+		int oldIndex = 0;
+		int spaceIndex = phrase.indexOf(' ');
+		while(oldIndex<phrase.length() && spaceIndex!=-1){
+			words.add(phrase.substring(oldIndex, spaceIndex));
+			oldIndex = spaceIndex+1;
+			spaceIndex = phrase.indexOf(' ', oldIndex);
+		}
+		
+		
+		for(int i=0; i<words.size(); i++){
+			String w = words.get(i).toLowerCase();
+			w = w.substring(0,1).toUpperCase() + w.substring(1);
+			correct += w + " ";
+		}
+		
+		return correct;	
+	}
 
-	
 	class Register implements ActionListener{
 
 		public void actionPerformed(ActionEvent event){
