541a804e32158ea65b97da7002f4294d0e50d6a5
Sahil Saini <sssaini011@gmail.com>
2016-03-18 12:34:40 -0400

    please ignore this i made some bad changes

--- GameShow/src/com/blueteam/gameshow/server/ServerAnswerMode.java
+++ GameShow/src/com/blueteam/gameshow/server/ServerAnswerMode.java
@@ -1,6 +1,9 @@
 package com.blueteam.gameshow.server;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
@@ -19,6 +22,8 @@ public class ServerAnswerMode extends JPanel implements ActionListener{
 	private JButton moveOn;
 	private ServerGameScreen SGS;
 	private Game game;
+	
+	private ArrayList<JLabel> allLabels;
 	
 	public ServerAnswerMode(Game g, ServerGameScreen s){	
 		SGS = s;
@@ -63,6 +68,64 @@ public class ServerAnswerMode extends JPanel implements ActionListener{
 			add(explanation);
 		
 		add(moveOn);
+		
+		allLabels = new ArrayList<JLabel>();
+		allLabels.add(question);
+		allLabels.add(explanation);
+		for(int y = 0; y< answerLabel.size(); y++){
+			
+			allLabels.add(answerLabel.get(y));
+			
+		}
+		
+		
+		
+		 addComponentListener(new ComponentAdapter() {
+	            public void componentResized(ComponentEvent e) {
+	            	
+	            	for(JLabel label: allLabels){
+	            	
+	            	Font labelFont = label.getFont();
+	            	
+	            	String labelText = label.getText();
+	            	
+	            	int stringWidth = label.getFontMetrics(labelFont).stringWidth(labelText);
+	            	
+	            	int componentWidth = label.getWidth();
+	            	
+	            	double widthRatio = (double)componentWidth / (double)stringWidth;
+	            	
+	            	int newFontSize = (int)(labelFont.getSize() * widthRatio);
+	            	
+	            	int componentHeight = label.getHeight();
+	            	
+	            	int fontSizeToUse = Math.min(newFontSize, componentHeight);
+	            	
+	            	label.setFont(new Font(labelFont.getName(), Font.PLAIN, fontSizeToUse));
+	             
+	            	}
+	            	
+	            	Font labelFont = moveOn.getFont();
+	            	
+	            	String labelText = moveOn.getText();
+	            	
+	            	int stringWidth = moveOn.getFontMetrics(labelFont).stringWidth(labelText);
+	            	
+	            	int componentWidth = moveOn.getWidth();
+	            	
+	            	double widthRatio = (double)componentWidth / (double)stringWidth;
+	            	
+	            	int newFontSize = (int)(labelFont.getSize() * widthRatio);
+	            	
+	            	int componentHeight = moveOn.getHeight();
+	            	
+	            	int fontSizeToUse = Math.min(newFontSize, componentHeight);
+	            	
+	            	moveOn.setFont(new Font(labelFont.getName(), Font.PLAIN, fontSizeToUse));
+	               
+	            }
+	        });
+			
 	}
 
 
