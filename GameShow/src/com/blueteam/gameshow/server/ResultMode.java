66bdb490f2d321e1d26793d771444db99b01c39e
CichlidHelen <CichlidHelen@Angel>
2016-03-20 21:15:48 -0400

    fixed font sizing in result mode

--- GameShow/src/com/blueteam/gameshow/server/ResultMode.java
+++ GameShow/src/com/blueteam/gameshow/server/ResultMode.java
@@ -1,9 +1,13 @@
 package com.blueteam.gameshow.server;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.*;
+
 import javax.swing.*;
+import javax.swing.table.JTableHeader;
+
 import com.blueteam.gameshow.data.Roster;
 
 public class ResultMode extends JPanel{
@@ -15,11 +19,20 @@ public class ResultMode extends JPanel{
 	private JButton back;
 	private JButton nextQ;
 	
+	int rowSize = 30;
+	
 	public ResultMode(Game g, final ServerGameScreen s){
 		rost = g.getRoster();
 		model = new ResultTableModel(rost);
 		table = new JTable(model);
-		table.setFillsViewportHeight(true);
+		table.setSize(new Dimension(200,200));
+		table.setTableHeader(new JTableHeader(table.getColumnModel()) {
+			@Override public Dimension getPreferredSize() {
+				Dimension d = super.getPreferredSize();
+				d.height = rowSize/3;
+				return d;
+			}
+		});
 		
 		back = new JButton("Back");
 		back.addActionListener(new ActionListener(){
@@ -45,7 +58,7 @@ public class ResultMode extends JPanel{
 		add(buttonPanel, BorderLayout.SOUTH);
 		
 		addComponentListener(new ComponentAdapter(){
-			public void ComponentResized(ComponentEvent e){
+			public void componentResized(ComponentEvent e){
 				resizeText();
 			}
 		});
@@ -72,23 +85,38 @@ public class ResultMode extends JPanel{
 				width = stringWidth;
 			}
 		}
-		return width;
+		
+		if(width<1){
+			width = 1;
+		}
+		
+		return (int) (width * 1.25);
 	}
 	
 	private void resizeText(){
-		int height = getHeight();
-		int rowSize = (int) ((double)height/model.getRowCount());
-
-		Font font = table.getFont();
+		int rowCount = model.getRowCount();
+		if(rowCount< 1){
+			rowCount = 1;
+		}
+		rowSize = (int) ((double)table.getHeight()/(double)rowCount);
+		table.setRowHeight(rowSize);
+		
+		Font font = table.getFont();	
 		double widthRatio = ((double)table.getWidth()/2) / ((double)getStringWidth(font));
 		int newFontSize = (int)(font.getSize() * widthRatio);
-		int componentHeight = table.getRowHeight();
-		int fontSizeToUse = Math.min(newFontSize, componentHeight);
-		font=new Font(font.getName(), Font.PLAIN, fontSizeToUse);
+		newFontSize = Math.min(newFontSize, rowSize);
+		font=new Font(font.getName(), Font.PLAIN, newFontSize);
 		if (font.getSize()<12)
 			font=new Font(font.getName(), Font.PLAIN, 12);
 		table.setFont(font);
 		
+		int stringWidth = table.getFontMetrics(font).stringWidth("Team Name");
+		widthRatio = ((double)table.getWidth()/2) / ((double)stringWidth*1.25);
+		newFontSize = (int)(font.getSize() * widthRatio);
+		newFontSize = Math.min(newFontSize, rowSize/3);
+		font=new Font(font.getName(), Font.PLAIN, newFontSize);
+		table.getTableHeader().setFont(font);
+
 	}
 	
 	
